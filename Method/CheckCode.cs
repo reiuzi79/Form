05734a2e74732f9443aa54825b9d5d1ed99cedf9
reiuzi79 <30955921+reiuzi79@users.noreply.github.com>
2019-05-02 22:58:40 +0800

    Add files via upload

---
+++ Method/CheckCode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 身份证信息管理系统
+{
+    public class CheckCode
+    {
+        public static string Code()
+        {
+            Random random = new Random();
+            int minV = 12345, maxV = 98765;
+            return random.Next(minV, maxV).ToString();
+        }
+    }
+}
